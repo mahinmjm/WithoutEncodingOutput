a3c19994022589cc28e411d888512c2b57c635f1
mahinmjm <mahinmjm@gmail.com>
2017-01-28 22:41:08 +0600

    first commit

---
+++ app/src/main/java/com/mahinjm/withoutencodingoutput/HtmlTagCheck.java
@@ -0,0 +1,14 @@
+package com.mahinjm.withoutencodingoutput;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by mahin on 1/28/17.
+ */
+
+public class HtmlTagCheck {
+
+
+
+}
